e54259265c4c861c87f32e00d079ed86b26bac6c
Jessica Torres <tjessica750@gmail.com>
2022-03-02 16:32:07 -0500

    commit operaciones

---
+++ archivo_prueba.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 13:41:58 2022
+
+@author: USER
+"""
+a = 3
+a = True
+a = "hola"
+print(type(a))
+print('Hola mundo')
+print("Hola de nuevo")
+
+# suma
+a = 5
+b = 2
+c = a+b
+print(c)
+
+a = 5
+b = 2
+c = a-b
+print(c)
+
+# multiplicacion
+a = 5
+b = 2
+c = a*b
+print(c)
+
+# division
+a = 5
+b = 2
+c = a/b
+print(c)
+
+# division parte entera
+a = 5
+b = 2
+c = a//b
+print(c)
+
+# potencia
+a = 2
+c = a**6
+print(c)
+
+#Raiz cuadrada
+a=16
+b=a**(1/2)
+
+import math
+raiz =math.sqrt(a)
+print(raiz)
+
+#String str
+a="Hola mundo"
+
